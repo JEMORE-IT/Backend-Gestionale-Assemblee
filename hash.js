0b562ec0bd9ba93de3ec0f7939801cd7f2bcb820
gabbonj <gabriele030403@gmail.com>
2024-12-23 11:23:11 +0100

    added terminal arguments for hash.js

--- hash.js
+++ hash.js
@@ -1,5 +1,16 @@
 const bcrypt = require('bcrypt');
 
-bcrypt.hash('password', 10, (err, hash) => {
-    console.log(hash)
-})
+const password = process.argv[2];
+
+if (!password) {
+    console.error('Per favore, fornisci una password come parametro.');
+    process.exit(1);
+}
+
+bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+        console.error('Errore durante la generazione dell\'hash:', err);
+        process.exit(1);
+    }
+    console.log('Password hashata:', hash);
+});
