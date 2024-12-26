c93e0c32ab8c4a1e6e1afc25c86b5d3d656694fa
gabbonj <gabriele030403@gmail.com>
2024-12-26 23:17:23 +0100

    password updater script now writes directly on .env file

--- hash.js
+++ hash.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const bcrypt = require('bcrypt');
 
 const password = process.argv[2];
@@ -13,4 +14,30 @@ bcrypt.hash(password, 10, (err, hash) => {
         process.exit(1);
     }
     console.log('Password hashata:', hash);
+
+    // Leggi il file .env
+    fs.readFile('.env', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Errore durante la lettura del file .env:', err);
+            process.exit(1);
+        }
+
+        let updatedEnv;
+        if (data.includes('PASSWORD=')) {
+            // Sostituisci il valore della variabile PASSWORD
+            updatedEnv = data.replace(/PASSWORD=.*/, `PASSWORD=${hash}`);
+        } else {
+            // Aggiungi la variabile PASSWORD
+            updatedEnv = `${data}\nPASSWORD=${hash}`;
+        }
+
+        // Scrivi il file .env aggiornato
+        fs.writeFile('.env', updatedEnv, 'utf8', (err) => {
+            if (err) {
+                console.error('Errore durante la scrittura del file .env:', err);
+                process.exit(1);
+            }
+            console.log('File .env aggiornato con successo.');
+        });
+    });
 });
